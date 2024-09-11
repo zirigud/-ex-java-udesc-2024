c84e48a1a78e8c21c17f7ae06ecc60bab8d685ca
Arthur Perret <thuiperret1@gmail.com>
2024-09-11 19:14:13 -0300

    Segundo commit

--- src/exercicios1/Ex2.java
+++ src/exercicios1/Ex2.java
@@ -1,4 +1,24 @@
 package exercicios1;
 
-public class Ex2 {
-}
+import java.util.Scanner;
+
+    public class Ex2 {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+
+            System.out.print("Digite a distância percorrida (em km): ");
+            int distancia = scanner.nextInt();
+
+
+            System.out.print("Digite o total de combustível gasto (em litros): ");
+            double combustivel = scanner.nextDouble();
+
+
+            double mediaConsumo = distancia / combustivel;
+
+
+            System.out.printf("A média de consumo do veículo é: %.2f km/l\n", mediaConsumo);
+        }
+    }
+
